592284e7c5b962b65cffacb47e6b75b5c0c9fb17
Joanna White <joanna1016@hotmail.com>
2019-03-15 02:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 };
 
 function deleteFromObjectByKey(object, key) {
-  
+
 };
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
